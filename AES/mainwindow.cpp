567e4520c0045289d15eaaf25c594d5ca2f6ebbc
fanrarie <2859936139@qq.com>
2025-03-30 14:02:30 +0800

    Add files via upload

---
+++ AES/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QToolBar>
+#include<QDebug>
+#include<QPushButton>
+#include<QWidget>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //重置窗口大小
+    resize(800,600);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_des_clicked()
+{
+    this->close();//关闭当前界面，即主界面
+    DES*pic=new DES();
+    pic->setWindowTitle("DES");
+    pic->resize(800,600);
+    pic->show();
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    this->close();//关闭当前界面，即主界面
+    AES*pic1=new AES();
+    pic1->setWindowTitle("AES");
+    pic1->resize(800,600);
+
+
+    pic1->show();
+}

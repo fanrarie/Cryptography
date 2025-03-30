225d4414690e25a7624fee250df8282fe930cff3
fanrarie <2859936139@qq.com>
2025-03-30 14:05:26 +0800

    Add files via upload

---
+++ RSA/mainwindow.cpp
@@ -0,0 +1,63 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QToolBar>
+#include<QDebug>
+#include<QPushButton>
+#include<QWidget>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //重置窗口大小
+    resize(800,600);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_des_clicked()
+{
+    this->close();//关闭当前界面，即主界面
+    DES*pic=new DES();
+    pic->setWindowTitle("DES");
+    pic->resize(800,600);
+    pic->show();
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    qDebug()<<"1";
+    this->close();//关闭当前界面，即主界面
+    AES*pic1=new AES();
+    pic1->setWindowTitle("AES");
+    pic1->resize(800,600);
+    qDebug()<<"1";
+
+
+    pic1->show();
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    qDebug()<<"6";
+    this->close();//关闭当前界面，即主界面
+    qDebug()<<"9";
+    RSA*pic2=new RSA();
+    pic2->setWindowTitle("RSA");
+    qDebug()<<"2";
+    pic2->resize(800,600);
+    if(pic2!=nullptr)
+    {
+
+          pic2->show();
+    }
+    else
+    {
+      qDebug()<<"pic2异常";
+    }
+}
